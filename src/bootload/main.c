da8bfb366094b1c95dd6ab79b924b6a9241581c5
Itaru Kitagawa <itaruxkita@gmail.com>
2018-04-12 20:28:15 +0900

    Clean up old code

--- src/bootload/main.c
+++ src/bootload/main.c
@@ -51,20 +51,6 @@ static void wait()
     ;
 }
 
-
-//int global_data = 0x10;
-//int global_bss;
-//static int static_data = 0x20;
-//static int static_bss;
-//
-//static void printval(void)
-//{
-//  puts("global_data = "); putxval(global_data, 0); puts("\n");
-//  puts("global_bss  = "); putxval(global_bss,  0); puts("\n");
-//  puts("static_data = "); putxval(static_data, 0); puts("\n");
-//  puts("static_bss  = "); putxval(static_bss,  0); puts("\n");
-//}
-
 int main(void)
 {
   static char buf[16];
@@ -104,19 +90,6 @@ int main(void)
       puts("unknown\n");
     }
   }
-
-//  puts("Hellow World\n");
-
-//  printval();
-//  puts("overwrite variables.\n");
-//  global_data = 0x20;
-//  global_bss = 0x30;
-//  static_data = 0x40;
-//  static_bss = 0x50;
-//  printval();
-  
-//  while (1)
-//    ;
 
   return 0;
 }
