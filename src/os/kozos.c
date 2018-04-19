b48a37efd348d53faccb59766f5f325cc891fbab
Itaru Kitagawa <itaruxkita@gmail.com>
2018-04-19 12:48:39 +0900

    8th step complete

--- src/os/kozos.c
+++ src/os/kozos.c
@@ -2,7 +2,7 @@
 #include "kozos.h"
 #include "intr.h"
 #include "interrupt.h"
-//#include "sysall.h"
+#include "syscall.h"
 #include "lib.h"
 
 #define THREAD_NUM 6
@@ -22,13 +22,13 @@ typedef struct _kz_thread {
   struct { /* スレッドのスタートアップ(thread_init())に渡すパラメータ */
     kz_func_t func; 
     int argc;
-    int **argv;
+    char **argv;
   } init;
 
-  //struct { /* システム・コール用バッファ */
-  //  kz_syscall_type_t type;
-  //  kz_syscall_param_t *param; 
-  //} syscall;
+  struct { /* システム・コール用バッファ */
+    kz_syscall_type_t type;
+    kz_syscall_param_t *param; 
+  } syscall;
 
   kz_context context; /* コンテキスト情報 */
 } kz_thread;
@@ -45,10 +45,41 @@ static kz_handler_t handlers[SOFTVEC_TYPE_NUM]; /* 割り込みハンドラ */
 
 void dispatch(kz_context *context);
 
+
+static int getcurrent(void)
+{
+  if (current == NULL) {
+    return -1;
+  }
+
+  readyque.head = current->next;
+  if (readyque.head == NULL) {
+    readyque.tail = NULL;
+  }
+  current->next = NULL;
+
+  return 0;
+}
+
+static int putcurrent(void)
+{
+  if (current == NULL) {
+    return -1;
+  }
+
+  if (readyque.tail) {
+    readyque.tail->next = current;
+  } else {
+    readyque.head = current;
+  }
+  readyque.tail = current;
+
+  return 0;
+}
+
 static void thread_end(void)
 {
-  puts("supposed to call kz_exit()...");
-  //kz_exit();
+  kz_exit();
 }
 
 /* スレッドのスタート・アップ */
@@ -115,15 +146,120 @@ static kz_thread_id_t thread_run(kz_func_t func, char *name, int stacksize, int 
   thp->context.sp = (uint32)sp;
 
   /* システム・コールを呼び出したスレッドをレディーキューに戻す */
-  //putcurrent();
+  putcurrent();
 
   /* 新規作成したスレッドをレディーキューに接続する */
   current = thp;
-  //putcurrent();
+  putcurrent();
 
   return (kz_thread_id_t)current;
 }
 
+static int thread_exit(void)
+{
+  /*
+   * 本来ならスタックも開放して再利用できるようにすべきだが省略
+   * このためスレッドを頻繁に生成・消去するようなことは現状できない
+   */
+  puts(current->name);
+  puts(" EXIT.\n");
+  memset(current, 0, sizeof(*current));
+  return 0;
+}
+
+/* 割り込みハンドラの登録*/
+static int setintr(softvec_type_t type, kz_handler_t handler)
+{
+  static void thread_intr(softvec_type_t type, unsigned long sp);
+
+  /*
+   * 割り込みを受け付けるために、ソフトウェア割り込みベクタに
+   * OSの割り込み処理の入り口となる関数を登録する
+   */
+  softvec_setintr(type, thread_intr);
+
+  handlers[type] = handler; /* OS側から呼び出す割り込みハンドラを登録 */
+
+  return 0;
+}
+
+static void call_functions(kz_syscall_type_t type, kz_syscall_param_t *p)
+{
+  /* システムコールの実行中に current が書き換わるので注意 */
+  switch (type) {
+    case KZ_SYSCALL_TYPE_RUN: /* kz_run() */
+      p->un.run.ret = thread_run( p->un.run.func, p->un.run.name,
+                                  p->un.run.stacksize,
+                                  p->un.run.argc, p->un.run.argv);
+      break;
+
+    case KZ_SYSCALL_TYPE_EXIT: /* kz_exit() */
+      /* TCB が消去されるので、戻り値を書き込んではいけない*/
+      thread_exit();
+      break;
+
+    default:
+      break;
+  }
+}
+
+static void syscall_proc(kz_syscall_type_t type, kz_syscall_param_t *p)
+{
+  /*
+   * システム・コールを呼び出したスレッドをレディーキューから
+   * 外した状態で処理関数を呼び出す。このためシステムコールを
+   * 呼び出したスレッドをそのまま動作継続させたい場合には
+   * 処理関数の内部で putcurrent() を行う必要がある。
+   */
+  getcurrent();
+  call_functions(type, p);
+}
+
+static void schedule(void)
+{
+  if (!readyque.head)
+    kz_sysdown();
+
+  current = readyque.head; 
+}
+
+static void syscall_intr(void)
+{
+  syscall_proc(current->syscall.type, current->syscall.param);
+}
+
+static void softerr_intr(void)
+{
+  puts(current->name);
+  puts(" DOWN.\n");
+  getcurrent(); /* レディーキューから外す */
+  thread_exit();
+}
+
+/* 割り込み処理の入口関数 */
+static void thread_intr(softvec_type_t type, unsigned long sp)
+{
+  /* カレンド・スレッドのコンテキストを保存する */
+  current->context.sp = sp;
+
+  /*
+   * 割り込みごとの処理を実行する
+   * SOFTVEC_TYPE_SYSCALL, SOFTVEC_TYPE_SOFTERR の場合は
+   * syscall_intr(), softerr_intr() がハンドラに登録されているので、
+   * それらが実行される
+   */
+  if (handlers[type])
+    handlers[type]();
+
+  schedule(); /* スレッドのスケジューリング */
+
+  /*
+   * スレッドのディスパッチ
+   * (dispatch() 関数の本体は startup.s にあり,アセンブラで記述されている)
+   */
+  dispatch(&current->context);
+  /* ここには返ってこない */
+}
 
 void kz_start(kz_func_t func, char *name, int stacksize, int argc, char *argv[])
 {
@@ -137,10 +273,9 @@ void kz_start(kz_func_t func, char *name, int stacksize, int argc, char *argv[])
   memset(threads, 0, sizeof(threads));
   memset(handlers, 0, sizeof(handlers));
 
-  //TODO: un-comment later
   /* 割り込みハンドラの登録 */
-  //setintr(SOFTVEC_TYPE_SYSCALL, syscall_intr); /* システム・コール */
-  //setintr(SOFTVEC_TYPE_SOFTERR, softerr_intr); /* ダウン要因発生 */
+  setintr(SOFTVEC_TYPE_SYSCALL, syscall_intr); /* システム・コール */
+  setintr(SOFTVEC_TYPE_SOFTERR, softerr_intr); /* ダウン要因発生 */
 
   //TODO: システム・コール不可なのはなぜ？
   /* システム・コール発行不可なので直接関数を呼び出してスレッド作成する */
@@ -150,4 +285,18 @@ void kz_start(kz_func_t func, char *name, int stacksize, int argc, char *argv[])
   dispatch(&current->context);
 
   /* ここには返ってこない */
+}
+
+void kz_sysdown(void)
+{
+  puts("system error!\n");
+  while (1)
+    ;
+}
+
+void kz_syscall(kz_syscall_type_t type, kz_syscall_param_t *param)
+{
+  current->syscall.type = type;
+  current->syscall.param = param;
+  asm volatile ("trapa #0"); /* トラップ割り込み発行 */
 }
--- src/os/main.c
+++ src/os/main.c
@@ -3,56 +3,11 @@
 #include "interrupt.h"
 #include "lib.h"
 
-//#include "defines.h"
-//#include "intr.h"
-//#include "interrupt.h"
-//#include "serial.h"
-//#include "lib.h"
-//
-//static void intr(softvec_type_t type, unsigned long sp)
-//{
-//  int c;
-//  static char buf[32];
-//  static int len;
-//
-//  c = getc();
-//
-//  if (c != '\n') {
-//    buf[len++] = c;
-//
-//  } else {
-//    buf[len++] = '\0';
-//
-//    if (!strncmp(buf, "echo", 4)) {
-//      puts(buf + 4);
-//      puts("\n");
-//    } else {
-//      puts("unknown.\n");
-//    }
-//    puts("> ");
-//    len = 0;
-//  }
-//}
-
+/* システム・タスクとユーザ・スレッドの起動 */
 static int start_threads(int argc, char *argv[])
 {
   puts("start_threads:beginning\n");
-
-  int cnt = 0;
-  int big_cnt = 0;
-  while (1) {
-    if (cnt > (10 * 1000)) {
-      puts("initial thread yay\n");
-      cnt = 0;
-      big_cnt++;
-    }
-    
-    if (big_cnt > 10){
-      puts("initial thread END!!\n");
-      break;
-    }
-    cnt++;
-  }
+  kz_run(test08_1_main, "command", 0x100, 0, NULL);
   return 0;
 }
 
@@ -67,18 +22,5 @@ int main(void)
   /* ここには戻ってこない */
 
   return 0;
-
-  //softvec_setintr(SOFTVEC_TYPE_SERINTR, intr);
-  //serial_intr_recv_enable(SERIAL_DEFAULT_DEVICE);
-
-  //puts("> ");
-  //
-  //INTR_ENABLE;
-
-  //while (1) {
-  //  asm volatile ("sleep"); /* 省電力モードに移行 */
-  //}
-
-  //return 0;
 }
 
